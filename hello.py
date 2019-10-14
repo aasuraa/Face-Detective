abea8e6ed7805ff2d09c6ce1129e427518b6716a
aasuraa <highbornsky@gmail.com>
2019-10-13 22:08:00 -0400

    after camera recording

--- hello.py
+++ hello.py
@@ -1,1 +1,16 @@
-print("Hello to you new
+import numpy as np
+import cv2
+
+vid = cv2.VideoCapture(0)
+
+while(True):
+	ret, frame = vid.read()
+	
+	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 						#only for grayscale image
+	
+	cv2.imshow('frame', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):									#quit with q
+		break
+	
+vid.release()
+cv2.destoryAllWindows()
