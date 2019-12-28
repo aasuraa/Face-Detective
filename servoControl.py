57f402c1348172bb9f1c564601250338e4ffc923
Sagar Ghimire <highbornsky@gmail.com>
2019-12-28 14:20:46 -0500

    Add files via upload
    updates

--- servoControl.py
+++ servoControl.py
@@ -9,6 +9,13 @@ GPIO.setwarnings(False)
 #p = GPIO.PWM(servoPIN, 50)
 i = 0
 p = start(i)
-p.ChangeDutyCycle(i)
-
+#p.ChangeDutyCycle(i)
+try:
+	while i < 11:
+		p.ChangeDutyCycle(i)
+		time.sleep(10)
+		i += 1
+except KeyboardInterrupt:
+	p,stop()
+	GPIO.cleanup()	
 GPIO.cleanup()
