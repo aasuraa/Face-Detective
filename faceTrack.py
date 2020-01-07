699c39871dde00c374672d74f208f791659febfa
Sagar Ghimire <highbornsky@gmail.com>
2020-01-07 14:50:00 -0500

    issues with video and servo interaction

--- faceTrack.py
+++ faceTrack.py
@@ -4,156 +4,146 @@ import RPi.GPIO as GPIO
 import time
 
 def moveServoPos(i, p):
-	'''
-		moves the servo positively to the right or down
-		args:
-			i:	duty cycle
-			p:	pwm
-		return:
-			i: 	duty cycle
-	'''
+    '''
+        moves the servo positively to the right or down
+        args:
+            i:  duty cycle
+            p:  pwm
+        return:
+            i:  duty cycle
+    '''
     p.ChangeDutyCycle(i)
-    #time.sleep(.09)
+    # time.sleep(.5)
+    cv2.waitKey(1000)
     return (i+.5)
     
 def moveServoNeg(i, p):
-	'''
-		moves the servo negatively to the left or up
-		args:
-			i:	duty cycle
-			p:	pwm
-		return:
-			i:	duty cycle
-	'''
+    '''
+        moves the servo negatively to the left or up
+        args:
+            i:  duty cycle
+            p:  pwm
+        return:
+            i:  duty cycle
+    '''
     p.ChangeDutyCycle(i)
-    #time.sleep(.09)
+    # time.sleep(.5)
+    cv2.waitKey(1000)
     return (i-.5)
+
+def verPosition(iVer):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    servoPIN = 17
+    GPIO.setup(servoPIN, GPIO.OUT)
+    p = GPIO.PWM(servoPIN, 50)
+    p.start(iVer)
+    time.sleep(1)			# add time delay to let servo process the code
+
+def position(i, iVer):
+    verPosition(iVer)
+
+    servoPIN = 22
+    GPIO.setup(servoPIN, GPIO.OUT)
+    q = GPIO.PWM(servoPIN, 50)
+    q.start(i)
+    print("Initial Position Set...")
+    time.sleep(3)
     
-def defaultServoMove(i, horzFlag, iVer=2.0): 
-	'''
-		takes the servo to the initial position and implements continuous horizontal movement
-		vertical servo is taken to initial position everytime face is lost
-		horizontal servo resumes the movement
-		args:
-			i:	duty cycle position, initial to the far right
-			horzFlag:	flag to move positively or negatively
-		return:			(necessary so it can resume pre-face detection movement)
-			i:	duty cycle positon
-			horzFlag:	flag for movement
-	'''   
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
+    GPIO.cleanup()
+    
+def mapServoFace(x, y, w, h):
+    '''
+        maps the servo position to face position
+        executes only if the face goes out of 90% of the frame boundary
+        args:
+            x, y:   face top left cordinates
+            w, h:   face width, height
+            i:      horizontal duty cycle value
+        return:
+			i:		horizontal duty cycle value to keep track of horizontal movement
+    '''
+    global i
+    global iVer
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    if (x < 0.1*fLen or x+w > 0.9*fLen):    # horizontal movement
+        GPIO.setup(22, GPIO.OUT)
+        p = GPIO.PWM(22, 50)
+        p.start(i)
+        if (x < 0.1*fLen):              # move left, neg movement, r
+            print("x<")
+            if i <= 2.0:
+                # print("Horizontal left limit")
+                i = 2.0
+            else:
+                i = moveServoPos(i, p)
+        else:                           # move right, pos movement, l
+            print("x+w>")
+            if i >= 12.0:
+                # print("Horizontal right limit")
+                i = 12.0
+            else:
+                i = moveServoNeg(i, p)
+    if (y < 0.1*fWid or y+h > 0.9*fWid):    # vertical movement
+        GPIO.setup(17, GPIO.OUT)
+        p = GPIO.PWM(17, 50)
+        p.start(iVer)
+        if (y < 0.1*fWid):              # move down, pos movement, u
+            print("y<")
+            if iVer <= 1.5:
+                # print("Vertical up limit")
+                iVer = 1.5
+            else:
+                iVer = moveServoNeg(iVer, p)
+        else:                           # move up, neg movement, d
+            print("y+h>")
+            if iVer >= 4.0:
+                # print("Vertical down limit")
+                iVer = 4.0
+            else:
+                iVer = moveServoPos(iVer, p)
+    print("Face mapped in frame")
+    # cv2.waitKey(2000)
+    GPIO.cleanup()
+    
+# global fLen 
+# global fWid 
 
-	servoPIN = 17
-	GPIO.setup(servoPIN, GPIO.OUT)
-	p = GPIO.PWM(servoPIN, 50)
-	p.start(iVer)
-	p.ChangeDutyCycle(iVer)
-
-	servoPIN = 22
-	GPIO.setup(servoPIN, GPIO.OUT)
-	p = GPIO.PWM(servoPIN, 50)
-	p.start(i)
-	p.ChangeDutyCycle(i)
-	try:
-		while i<=13.0 and i>=1.0:
-			if horzFlag == True:
-				i = moveServoPos(i, p)
-				if i > 12.0:
-					horzFlag = False
-			else:
-				i = moveServoNeg(i, p)
-				if i < 2.0:
-					horzFlag = True
-			
-			# check vidoe for face
-			ret, frame = vid.read()
-			# TODO: show the video even though face is not in the frame
-			# cv2.imshow('frame', frame)
-			faces = faceCascade.detectMultiScale(frame, scaleFactor=1.2, minNeighbors=5, minSize=(20,20))
-			if(len(faces) != 0):
-				return i, horzFlag
-	except KeyboardInterrupt:
-		p.stop()
-		GPIO.cleanup()  
-	# TODO: check to see if cheaning up everytime is necessary
-	GPIO.cleanup()
-
-def positionServo(x, y, w, h, i):
-	'''
-		maps the servo position to face position
-		executes only if the face goes out of 90% of the frame boundary
-		args:
-			x, y:	face top left cordinates
-			w, h:	face width, height
-			i: 		duty cycle value
-	'''
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	if (x < 0.1*fLen or x+w > 0.9*fLen):	# horizontal movement
-		GPIO.setup(22, GPIO.OUT)
-		p = GPIO.PWM(22, 50)
-		p.start(i)
-		if (x < 0.1*fLen):				# move left, neg movement, r
-			return moveServoPos(i, p)
-			# print("x<")
-		else:							# move right, pos movement, l
-			return moveServoNeg(i, p)
-			# print("x+w>")
-	if (y < 0.1*fWid or y+h > 0.9*fWid):	# vertical movement
-		GPIO.setup(17, GPIO.OUT)
-		p = GPIO.PWM(17, 50)
-		p.start(i)
-		if (y < 0.1*fWid):				# move down, pos movement, u
-			iVer = moveServoPos(iVer, p)
-			return i
-			# print("y<")
-		else:							# move up, neg movement, d
-			iVer = moveServoNeg(iVer, p)
-			return i
-			# print("y+h>")
-
-'''
-    GPIO BCM = 17 for vertical movement
-    GPIO BCM = 22 for horizontal movement
-'''
-# TODO: make use of global variables for pins
-# length and width of the video capture frame
-global fLen 
-global fWid 
-global iVer
-
-i = 2.0 	# initial servo position
-iVer = 2.0	# initial vertical servo position, servo has to come back to this
-horzFlag = True 	# initial servo movement direction
+i = 10.0     # initial horizontal servo position
+iVer = 1.5	# initial vertical servo position
+horzFlag = True     # initial servo movement direction
 
 faceCascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
 
-vid = cv2.VideoCapture(0)													#initializing the video capture
+vid = cv2.VideoCapture(0)                                                   #initializing the video capture
+vid.read()
 fLen = vid.get(3)
 fWid = vid.get(4)
-	
-while(cap.isOpened()):
+
+cv2.waitKey(2000)
+position(i, iVer)
+
+while(vid.isOpened()):
+	# cv2.waitKey(5000)
 	ret, frame = vid.read()
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 						#only for grayscale image
-	
-	#face detection
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                      #only for grayscale image
+    
+    #face detection
 	faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20,20))
-	
-	if len(faces) == 0: 									# no face is detected
-		i, horzFlag = defaultServoMove(i, horzFlag)	
-	else:
-		#rectangular box if a face is detected
-		for (x, y, w, h) in faces:
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-			roi_color = frame[y:y+h, x:x+w]
-			if( (x < 0.1*fLen) or (y < 0.1*fWid) or (x+w > 0.9*fLen) or (y+h > 0.9*fWid)):
-				if i<=12.0 and i>=2.0:
-					i = positionServo(x, y, w, h, i)
-	
+    
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+		roi_color = frame[y:y+h, x:x+w]
+		if( (x < 0.1*fLen) or (y < 0.1*fWid) or (x+w > 0.9*fLen) or (y+h > 0.9*fWid)):
+			# if (i>=2.0 and i<=12.0) or (iVer >= 1.5 and iVer<= 4.0):
+			mapServoFace(x, y, w, h)
+    
 	cv2.imshow('frame', cv2.flip(frame, 1))
-	if cv2.waitKey(1) & 0xFF == ord('q'):									# quit with q
+	if cv2.waitKey(1) & 0xFF == ord('q'):                                   # quit with q
 		break
-	
+    
 vid.release()
 cv2.destoryAllWindows()
